a75dc3724053f62d436ec1607e28c96a937a9883
Jagermeister56 <jamesiasenza@gmail.com>
2023-12-15 13:02:30 -0500

    Update api.php
    debugs

--- api.php
+++ api.php
@@ -11,161 +11,127 @@ const ZEC_ADDRESS = 't1ef9cxzpToGJcaSMXbTGRUDyrp76GfDLJG';
 const LTC_ADDRESS = 'LR8bPo7NjPNRVy6nPLVgr9zHee2C7RepKA';
 const USDTE_ADDRESS = '0xD36591b20f738f6929272a4391B8C133CB2e5C96';
 
-
 const CACHE_TEMPLATE = __DIR__ . '/cache/%s.cache';
 
+function debugLog($message) {
+    error_log(print_r($message, true));
+}
+
 function getCache($key) {
+    debugLog("getCache called for key: " . $key);
     $cache_file = sprintf(CACHE_TEMPLATE, $key);
     if (file_exists($cache_file) && (filemtime($cache_file) > (time() - 60))) {
+        debugLog("Cache hit for key: " . $key);
         return file_get_contents($cache_file);
     }
-
+    debugLog("Cache miss for key: " . $key);
     return false;
 }
 
 function setCache($key, $value) {
-    file_put_contents(
-        sprintf(CACHE_TEMPLATE, $key),
-        $value,
-        LOCK_EX
-    );
+    debugLog("setCache called for key: " . $key . " with value: " . $value);
+    file_put_contents(sprintf(CACHE_TEMPLATE, $key), $value, LOCK_EX);
 }
 
-
 function getCoinPrice($coin) {
-
-    #if ($cache = getCache($coin.'-price')) {
-    #    return $cache;
-    #}
-
+    debugLog("getCoinPrice called for coin: " . $coin);
     $data = file_get_contents('https://api.coingecko.com/api/v3/simple/price?ids='.$coin.'&vs_currencies=usd');
     if ($data === false) {
+        debugLog("Failed to retrieve data for coin price: " . $coin);
         return null;
     }
-
-
     $data = json_decode($data);
-    #setCache($coin.'-price', $data->bitcoinz->usd);
-
     return (float)($data->$coin->usd);
 }
 
-function getBtczBalance()
-{
-    if ($cache = getCache('btcz-balance')) {
-        return $cache;
-    }
-
+function getBtczBalance() {
+    debugLog("getBtczBalance called");
     $total = 0;
     foreach (BTCZ_ADDRESSES as $address) {
+        debugLog("Fetching balance for BTCZ address: " . $address);
         $addressTotal = file_get_contents('https://explorer.btcz.rocks/api/addr/' . $address . '/balance');
-
-
+        if ($addressTotal === false) {
+            debugLog("Failed to fetch balance for BTCZ address: " . $address);
+            continue;
+        }
         $total += $addressTotal;
     }
-
     $total = $total / 1000000000000000000;
+    debugLog("Total BTCZ balance: " . $total);
     setCache('btcz-balance', $total);
-
     return $total;
 }
 
 function getEthBalance() {
-    const API_KEY = 'NIEKSBV3HT23UCI2ATHA5M57VVS5UWY9TF';
-
-    if ($cache = getCache('eth-balance')) {
-        return $cache;
-    }
-
+    debugLog("getEthBalance called");
     $url = 'https://api.etherscan.io/api?module=account&action=balance&address='. ETH_ADDRESS .'&tag=latest&apikey=' . API_KEY;
     $data = file_get_contents($url);
-
     if ($data === false) {
+        debugLog("Failed to fetch ETH balance");
         return null;
     }
-
     $data = json_decode($data, true);
     if ($data['status'] !== "1") {
+        debugLog("Error in ETH balance response");
         return null;
     }
-
     $balanceInWei = $data['result'];
-    $balanceInEth = bcdiv($balanceInWei, '1000000000000000000', 18); 
-
+    $balanceInEth = bcdiv($balanceInWei, '1000000000000000000', 18);
+    debugLog("ETH balance: " . $balanceInEth);
     setCache('eth-balance', $balanceInEth);
-
     return $balanceInEth;
 }
 
-
-function getBtcBalance()
-{
-    if ($cache = getCache('btc-balance')) {
-        return $cache;
-    }
-
+function getBtcBalance() {
+    debugLog("getBtcBalance called");
     $data = file_get_contents('http://blockchain.info/q/addressbalance/' . BTC_ADDRESS);
     if ($data === false) {
+        debugLog("Failed to fetch BTC balance");
         return null;
     }
-
-    $data = $data/100000000;
-
+    $data = $data / 100000000;
+    debugLog("BTC balance: " . $data);
     setCache('btc-balance', $data);
-
     return $data;
 }
 
-function getZecBalance()
-{
-    if ($cache = getCache('zec-balance')) {
-        return $cache;
-    }
-
+function getZecBalance() {
+    debugLog("getZecBalance called");
     $data = file_get_contents('https://api.zcha.in/v2/mainnet/accounts/' . ZEC_ADDRESS);
-
     if (!$data) {
+        debugLog("Failed to fetch ZEC balance");
         return null;
     }
-
     $data = json_decode($data);
+    debugLog("ZEC balance: " . $data->balance);
     setCache('zec-balance', $data->balance);
-
     return $data->balance;
 }
 
-function getLtcBalance()
-{
-    if ($cache = getCache('ltc-balance')) {
-        return $cache;
-    }
-
+function getLtcBalance() {
+    debugLog("getLtcBalance called");
     $data = file_get_contents('https://chainz.cryptoid.info/ltc/api.dws?q=getbalance&a=' . LTC_ADDRESS);
     if ($data === false) {
+        debugLog("Failed to fetch LTC balance");
         return null;
     }
-
+    debugLog("LTC balance: " . $data);
     setCache('ltc-balance', $data);
-
     return $data;
 }
-function getUSDTEBalance()
-{
-    if ($cache = getCache('USDTE-balance')) {
-        return $cache;
-    }
-    
-    $address = USDTE_ADDRESS;
-    $data = json_decode(file_get_contents("https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xdac17f958d2ee523a2206206994597c13d831ec7&address={$address}"));
 
+function getUSDTEBalance() {
+    debugLog("getUSDTEBalance called");
+    $data = json_decode(file_get_contents("https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xdac17f958d2ee523a2206206994597c13d831ec7&address=" . USDTE_ADDRESS));
     if ((!isset($data->status) && $data->status !== 1) || $data === false) {
+        debugLog("Failed to fetch USDTE balance");
         return null;
     }
-
+    debugLog("USDTE balance: " . ($data->result * 0.000001));
     setCache('USDTE-balance', $data->result * 0.000001);
-
     return $data->result * 0.000001;
 }
+
 $response = [
     'btczBalance' => getBtczBalance(),
     'ethBalance' => getEthBalance(),
@@ -181,4 +147,5 @@ $response = [
     'USDTEUsd' => getCoinPrice('tether')
 ];
 
+debugLog("Response: " . json_encode($response));
 echo json_encode($response);
